b26d3058101c2811835af44beac3e06c2b7b6e59
Jay Butera <buterajay@gmail.com>
2018-11-03 23:26:15 +0100

    Annoying sounds included

--- index.jsx
+++ index.jsx
@@ -1,20 +1,20 @@
 import { h, app } from 'hyperapp'
-import Web3 from 'web3'
+//import Web3 from 'web3'
 
 //const web3 = new Web3(web3.currentProvider)
 
-const state = {
-   count: 0
-}
+const state = {}
 
 const actions = {
-   inc: () => state => ({ count: state.count + 1 })
+   inc: () => state => { document.getElementById('chaching').play() }
 }
 
 const view = (state, actions) => (
    <div>
-      <h1>{ state.count }</h1>
-      <input type="image" src="https://investorplace.com/wp-content/uploads/2013/12/dogecoin.png" onclick
+      <audio id="chaching" preload="auto">
+         <source src="money.mp3" />
+      </audio>
+      <input type="image" src="https://investorplace.com/wp-content/uploads/2013/12/dogecoin.png" onclick={()=>actions.inc()} />
    </div>
 )
 
