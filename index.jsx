8ec51d8cd6a6289c7c03640c81db26853f029b40
Jay Butera <buterajay@gmail.com>
2018-11-04 11:01:19 +0100

    Query token balance

--- index.jsx
+++ index.jsx
@@ -1,9 +1,13 @@
 import { h, app } from 'hyperapp'
 import Web3 from 'web3'
+import ETabi from './abis/ExampleToken.json'
+import TransmuteAbi from './abis/Transmute.json'
+
+const ETaddr = '0xb863e9df9c9b860d3a8fdec4298fc1c8d8b4fc8a'
+const TransmuteAddr = '0xbe99cd10447142431d312cdde568789548fef4f5'
 
 const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider("http://localhost:8545"))
 console.log(Web3.givenProvider)
-//web3.eth.getAccounts().then(console.log)
 
 const state = {
    token: 0,
@@ -15,10 +19,16 @@ const actions = {
    coin_bal: () => async (state, actions) => {
       const addr = (await web3.eth.getAccounts())[0]
       const bal  = Number(web3.utils.fromWei(await web3.eth.getBalance(addr), 'ether'))
-      actions.u_token( bal )
+      actions.u_coin( bal )
    },
    //u: (x) => state => (x)
-   u_coin: (x) => state => ({ coin: x })
+   u_coin: (x) => state => ({ coin: x }),
+   token_bal: () => async (state, actions) => {
+      const addr = (await web3.eth.getAccounts())[0]
+      const token = new web3.eth.Contract(ETabi, ETaddr)
+      actions.u_token(await token.methods.balanceOf(addr).call())
+   },
+   u_token: (x) => state => ({ token: x }),
 }
 
 const view = (state, actions) => (
@@ -31,6 +41,8 @@ const view = (state, actions) => (
    </center>
    <button onclick={()=>actions.coin_bal()}>Do it</button>
    { state.coin }
+   <button onclick={()=>actions.token_bal()}>You wont</button>
+   { state.token }
    </div>
 )
 
